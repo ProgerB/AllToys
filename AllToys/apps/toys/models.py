d14f5b37e1ffaef57109b985095ec438439d21e8
ProgerB <xurramovbahrom@mail.ru>
2020-12-06 23:38:26 +0500

    Create Toy model migrations

--- AllToys/apps/toys/models.py
+++ AllToys/apps/toys/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class ActiveObjectManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_active=True)
+
+
+class User(models.Model):
+    is_active = models.BooleanField(default=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    objects = models.Manager()
+
+    active_objects = ActiveObjectManager()
+
+
+class Toy(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, related_name="toys", on_delete=models.CASCADE, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
--- AllToys/apps/toys/urls.py
+++ AllToys/apps/toys/urls.py
@@ -4,4 +4,5 @@ from toys import views
 
 urlpatterns = [
     path('', views.dashboard, name="dashboard"),
+    path("update_server/", views.update, name="update"),
 ]
